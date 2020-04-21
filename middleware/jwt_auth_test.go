9e30cca586f147f403e5f38564a40c69ca8781b9
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-04-21 10:18:03 +0100

    SP-319 - Added test cases to cover expired token and wrong siging method

--- middleware/jwt_auth_test.go
+++ middleware/jwt_auth_test.go
@@ -7,10 +7,6 @@ import (
 	"testing"
 )
 
-type Msg struct {
-	Message string `json:"message"`
-}
-
 func TestJwtVerify(t *testing.T) {
 	token := "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODcwNTIzMTcsImV4cCI6OTk5OTk5OTk5OSwic2Vzc2lvbi1kYXRhIjoiVGVzdC5NY1Rlc3RGYWNlQG1haWwuY29tIn0.8HtN6aTAnE2YFI9rJD8drzqgrXPkyUbwRRJymcPSmHk"
 	req, err := http.NewRequest("GET", "/jwt", nil)
@@ -29,7 +25,6 @@ func TestJwtVerify(t *testing.T) {
 	if rw.Result().StatusCode == 403 {
 		t.Fatalf("Status code should be 200 as a valid JWT token was passed")
 	}
-
 }
 
 func TestJwtVerifyInvalidToken(t *testing.T) {
@@ -48,6 +43,7 @@ func TestJwtVerifyInvalidToken(t *testing.T) {
 	handler := JwtVerify(testHandler)
 	handler.ServeHTTP(rw, req)
 	fmt.Println(rw.Result())
+
 	if rw.Result().StatusCode == 200 {
 		t.Fatalf("Status code should be 403 as a invalid JWT token was passed")
 	}
@@ -71,4 +67,46 @@ func TestJwtVerifyNoJwtToken(t *testing.T) {
 	if rw.Result().StatusCode == 200 {
 		t.Fatalf("Status code should be 403 as no JWT token was passed")
 	}
+}
+
+func TestJwtVerifyWrongSigningMethod(t *testing.T) {
+	token := "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.POstGetfAytaZS82wHcjoTyoqhMyxXiWdR7Nn7A29DNSl0EiXLdwJ6xC6AfgZWF1bOsS_TuYI3OG85AmiExREkrS6tDfTQ2B3WXlrr-wp5AokiRbz3_oB4OxG-W9KcEEbDRcZc0nH3L7LzYptiy1PtAylQGxHTWZXtGz4ht0bAecBgmpdgXMguEIcoqPJ1n3pIWk_dUZegpqx0Lka21H6XxUTxiy8OcaarA8zdnPUnV6AmNP3ecFawIFYdvJB_cm-GvpCSbr8G8y_Mllj8f4x9nBH8pQux89_6gUY618iYv7tuPWBFfEbLxtF2pZS6YC1aSfLQxeNe8djT9YjpvRZA"
+
+	req, err := http.NewRequest("GET", "/jwt", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", token)
+
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	rw := httptest.NewRecorder()
+	handler := JwtVerify(testHandler)
+	handler.ServeHTTP(rw, req)
+	fmt.Println(rw.Result())
+	if rw.Result().StatusCode == 200 {
+		t.Fatalf("Status code should be 403 as a token with the wrong siging method was passed")
+	}
+}
+
+func TestJwtVerifyExpiredToken(t *testing.T) {
+	token := "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODcwNTIzMTcsImV4cCI6MTU4NzA1MjMxNywic2Vzc2lvbi1kYXRhIjoiVGVzdC5NY1Rlc3RGYWNlQG1haWwuY29tIn0.OuafGwOMHkXrFiQFrog8-zR14hxRwFkq5SeWXgvKi2o"
+	req, err := http.NewRequest("GET", "/jwt", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", token)
+
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	rw := httptest.NewRecorder()
+	handler := JwtVerify(testHandler)
+	handler.ServeHTTP(rw, req)
+
+	if rw.Result().StatusCode == 200 {
+		t.Fatalf("Status code should be 403 as a expired JWT token was passed")
+	}
+
 }
