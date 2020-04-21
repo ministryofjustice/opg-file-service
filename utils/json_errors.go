c6930eb69c1a80ce005d41dccaa6aa58bdee464d
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-04-21 16:02:30 +0100

    SP-319 - Create util function to handle json error messages, pull and hash user email then pass it to handler in context

---
+++ utils/json_errors.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type jsonError struct {
+	Error       string `json:"error"`
+	Description string `json:"error_description"`
+}
+
+func WriteJSONError(w http.ResponseWriter, error string, description string, statusCode int) {
+	w.WriteHeader(statusCode)
+	if err := json.NewEncoder(w).Encode(jsonError{
+		Error:       error,
+		Description: description,
+	}); err != nil {
+		log.Println("handler/middleware failed to write response:", err)
+	}
+}
