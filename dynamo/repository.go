a00952cb4b5470589c93eb5e6b2800808a9f51ad
devkokov <dm.kokov@gmail.com>
2020-04-22 10:24:41 +0100

    SW-2307 remove debug from Dynamo repository

--- dynamo/repository.go
+++ dynamo/repository.go
@@ -2,7 +2,6 @@ package dynamo
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"log"
@@ -27,54 +26,6 @@ func NewRepository(sess *session.Session, l *log.Logger) *Repository {
 	return &Repository{
 		db:     dynamo,
 		logger: l,
-	}
-}
-
-// TODO: remove ListTables method when done debugging
-func (repo Repository) ListTables() {
-	// create the input configuration instance
-	table := "zip-requests"
-	input := &dynamodb.ListTablesInput{
-		ExclusiveStartTableName: &table,
-	}
-
-	for {
-		// Get the list of tables
-		repo.logger.Println("endpoint is " + repo.db.Endpoint)
-		repo.logger.Println("about to get list of tables...")
-
-		result, err := repo.db.ListTables(input)
-
-		repo.logger.Println(result.TableNames)
-
-		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
-				case dynamodb.ErrCodeInternalServerError:
-					repo.logger.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-				default:
-					repo.logger.Println(aerr.Error())
-				}
-			} else {
-				// Print the error, cast err to awserr.Error to get the Code and
-				// Message from an error.
-				repo.logger.Println(err.Error())
-			}
-			return
-		}
-
-		for _, n := range result.TableNames {
-			repo.logger.Println(*n)
-		}
-
-		// assign the last read tablename as the start for our next call to the ListTables function
-		// the maximum number of table names returned in a call is 100 (default), which requires us to make
-		// multiple calls to the ListTables function to retrieve all table names
-		input.ExclusiveStartTableName = result.LastEvaluatedTableName
-
-		if result.LastEvaluatedTableName == nil {
-			break
-		}
 	}
 }
 
