3fff7cdf3060b6e8d9916448b5f672dad67a3d7d
devkokov <dm.kokov@gmail.com>
2020-04-22 13:50:38 +0100

    SW-2307 generate a safe filepath for the file within the zip

--- storage/file.go
+++ storage/file.go
@@ -1,7 +1,34 @@
 package storage
 
+import (
+	"regexp"
+	"strings"
+)
+
 type File struct {
 	S3path   string
 	FileName string
 	Folder   string
+}
+
+func (f *File) getSafeFileName() string {
+	regex := regexp.MustCompile(`[#<>:"/\|?*\\]`)
+	safe := regex.ReplaceAllString(f.FileName, "")
+	if safe == "" {
+		return "file" // fallback
+	}
+	return safe
+}
+
+func (f *File) GetZipPath() string {
+	path := ""
+
+	if f.Folder != "" {
+		path += f.Folder
+		if !strings.HasSuffix(path, "/") {
+			path += "/"
+		}
+	}
+
+	return path + f.getSafeFileName()
 }
