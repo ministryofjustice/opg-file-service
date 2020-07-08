8c5de28fea788a3193d316dcbbe904470b657b0a
Joshua Hawxwell <m@hawx.me>
2020-07-08 13:59:25 +0100

    Improve zip handler tests
    - Use pointer receiver for mocks since they embed a mutex
    - Specify struct names so they are readable and zero-values can be omitted
    - Use t.Run()

--- handlers/zip_test.go
+++ handlers/zip_test.go
@@ -48,160 +48,106 @@ func TestZipHandler_ServeHTTP(t *testing.T) {
 		wantInLog    []string
 	}{
 		{
-			"No ref token passed in URL",
-			"",
-			"",
-			0,
-			nil,
-			nil,
-			0,
-			nil,
-			0,
-			nil,
-			0,
-			0,
-			nil,
-			404,
-			[]string{},
+			scenario:  "no_ref",
+			wantCode:  404,
+			wantInLog: []string{},
 		},
 		{
-			"Ref token not found",
-			"test",
-			"",
-			1,
-			nil,
-			storage.NotFoundError{Ref: "test"},
-			0,
-			nil,
-			0,
-			nil,
-			0,
-			0,
-			nil,
-			404,
-			[]string{
+			scenario:     "ref_not_found",
+			ref:          "test",
+			repoGetCalls: 1,
+			repoGetErr:   storage.NotFoundError{Ref: "test"},
+			wantCode:     404,
+			wantInLog: []string{
 				"Zip files for reference: test",
 				storage.NotFoundError{Ref: "test"}.Error(),
 			},
 		},
 		{
-			"Ref token has expired",
-			"test",
-			"",
-			1,
-			&storage.Entry{
+			scenario:     "ref_expired",
+			ref:          "test",
+			repoGetCalls: 1,
+			repoGetOut: &storage.Entry{
 				Ref: "test",
 				Ttl: 0,
 			},
-			nil,
-			0,
-			nil,
-			0,
-			nil,
-			0,
-			0,
-			nil,
-			404,
-			[]string{
+			wantCode: 404,
+			wantInLog: []string{
 				"Reference token 'test' has expired",
 			},
 		},
 		{
-			"Ref token does not belong to authenticated user",
-			"test",
-			"user",
-			1,
-			&storage.Entry{
+			scenario:     "ref_does_not_belong_to_user",
+			ref:          "test",
+			userHash:     "user",
+			repoGetCalls: 1,
+			repoGetOut: &storage.Entry{
 				Ref:  "test",
 				Hash: "otherUser",
 				Ttl:  9999999999,
 			},
-			nil,
-			0,
-			nil,
-			0,
-			nil,
-			0,
-			0,
-			nil,
-			403,
-			[]string{
+			wantCode: 403,
+			wantInLog: []string{
 				"Access denied for user: user",
 			},
 		},
 		{
-			"Unable to zip one of the files",
-			"test",
-			"",
-			1,
-			&storage.Entry{
+			scenario:     "unable_to_zip_file",
+			ref:          "test",
+			repoGetCalls: 1,
+			repoGetOut: &storage.Entry{
 				Ref:   "test",
 				Ttl:   9999999999,
 				Files: []storage.File{{}},
 			},
-			nil,
-			0,
-			nil,
-			1,
-			errors.New("error adding file to zip"),
-			1,
-			0,
-			nil,
-			500,
-			[]string{
+			addFileCalls: 1,
+			addFileErr:   errors.New("error adding file to zip"),
+			openCalls:    1,
+			wantCode:     500,
+			wantInLog: []string{
 				"error adding file to zip",
 			},
 		},
 		{
-			"Error when closing zip",
-			"test",
-			"",
-			1,
-			&storage.Entry{
+			scenario:     "error_on_closing_zip",
+			ref:          "test",
+			repoGetCalls: 1,
+			repoGetOut: &storage.Entry{
 				Ref: "test",
 				Ttl: 9999999999,
 			},
-			nil,
-			1,
-			nil,
-			0,
-			nil,
-			1,
-			1,
-			errors.New("some error when closing zip"),
-			200,
-			[]string{
+			repoDelCalls: 1,
+			openCalls:    1,
+			closeCalls:   1,
+			closeErr:     errors.New("some error when closing zip"),
+			wantCode:     200,
+			wantInLog: []string{
 				"some error when closing zip",
 			},
 		},
 		{
-			"Error when deleting entry from DB after it has been processed",
-			"test",
-			"",
-			1,
-			&storage.Entry{
+			scenario:     "error_deleting_entry_from_db",
+			ref:          "test",
+			repoGetCalls: 1,
+			repoGetOut: &storage.Entry{
 				Ref: "test",
 				Ttl: 9999999999,
 			},
-			nil,
-			1,
-			errors.New("some error deleting entry"),
-			0,
-			nil,
-			1,
-			1,
-			nil,
-			200,
-			[]string{
+			repoDelCalls: 1,
+			repoDelErr:   errors.New("some error deleting entry"),
+			openCalls:    1,
+			closeCalls:   1,
+			wantCode:     200,
+			wantInLog: []string{
 				"some error deleting entry",
 			},
 		},
 		{
-			"Successfully zip multiple files",
-			"test",
-			"user",
-			1,
-			&storage.Entry{
+			scenario:     "success",
+			ref:          "test",
+			userHash:     "user",
+			repoGetCalls: 1,
+			repoGetOut: &storage.Entry{
 				Ref:  "test",
 				Hash: "user",
 				Ttl:  9999999999,
@@ -218,61 +164,95 @@ func TestZipHandler_ServeHTTP(t *testing.T) {
 					},
 				},
 			},
-			nil,
-			1,
-			nil,
-			2,
-			nil,
-			1,
-			1,
-			nil,
-			200,
-			[]string{
+			repoDelCalls: 1,
+			addFileCalls: 2,
+			openCalls:    1,
+			closeCalls:   1,
+			wantCode:     200,
+			wantInLog: []string{
 				"Request took:",
 			},
 		},
 	}
 
 	for _, test := range tests {
-		mr := new(MockRepository)
-		mz := new(MockZipper)
-		buf := new(bytes.Buffer)
-		l := log.New(buf, "test", log.LstdFlags)
+		t.Run(test.scenario, func(t *testing.T) {
+			w := httptest.NewRecorder()
 
-		zh := ZipHandler{
-			repo:   mr,
-			zipper: mz,
-			logger: l,
-		}
+			var buf bytes.Buffer
+			l := log.New(&buf, "test", log.LstdFlags)
 
-		sm := mux.NewRouter()
-		sm.Handle("/zip/{reference}", &zh)
+			mr := &MockRepository{}
+			mr.On("Get", test.ref).Return(test.repoGetOut, test.repoGetErr).Times(test.repoGetCalls)
+			mr.On("Delete", test.repoGetOut).Return(test.repoDelErr).Times(test.repoDelCalls)
 
-		req, err := http.NewRequest("GET", "/zip/"+test.ref, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
+			mz := &MockZipper{}
+			mz.On("Open", w).Return().Times(test.openCalls)
+			mz.On("Close").Return(test.closeErr).Times(test.closeCalls)
 
-		rr := httptest.NewRecorder()
-		ctx := context.WithValue(req.Context(), hashedEmail{}, test.userHash)
+			sm := mux.NewRouter()
+			sm.Handle("/zip/{reference}", &ZipHandler{
+				repo:   mr,
+				zipper: mz,
+				logger: l,
+			})
 
-		mr.On("Get", test.ref).Return(test.repoGetOut, test.repoGetErr).Times(test.repoGetCalls)
-		mr.On("Delete", test.repoGetOut).Return(test.repoDelErr).Times(test.repoDelCalls)
+			req, err := http.NewRequest("GET", "/zip/"+test.ref, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		mz.On("Open", rr).Return().Times(test.openCalls)
-		mz.On("Close").Return(test.closeErr).Times(test.closeCalls)
+			ctx := context.WithValue(req.Context(), hashedEmail{}, test.userHash)
 
-		if test.addFileCalls > 0 {
-			mz.On("AddFile", mock.AnythingOfType("*storage.File")).Return(test.addFileErr).Times(test.addFileCalls)
-		}
+			if test.addFileCalls > 0 {
+				mz.On("AddFile", mock.AnythingOfType("*storage.File")).Return(test.addFileErr).Times(test.addFileCalls)
+			}
 
-		sm.ServeHTTP(rr, req.WithContext(ctx))
-		res := rr.Result()
+			sm.ServeHTTP(w, req.WithContext(ctx))
+			resp := w.Result()
 
-		for _, ls := range test.wantInLog {
-			assert.Contains(t, buf.String(), ls, test.scenario)
-		}
+			for _, ls := range test.wantInLog {
+				assert.Contains(t, buf.String(), ls, test.scenario)
+			}
 
-		assert.Equal(t, test.wantCode, res.StatusCode, test.scenario)
+			assert.Equal(t, test.wantCode, resp.StatusCode, test.scenario)
+		})
 	}
+}
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (m *MockRepository) Get(ref string) (*storage.Entry, error) {
+	args := m.Called(ref)
+	return args.Get(0).(*storage.Entry), args.Error(1)
+}
+
+func (m *MockRepository) Delete(entry *storage.Entry) error {
+	args := m.Called(entry)
+	return args.Error(0)
+}
+
+func (m *MockRepository) Add(entry *storage.Entry) error {
+	args := m.Called(entry)
+	return args.Error(0)
+}
+
+type MockZipper struct {
+	mock.Mock
+}
+
+func (m *MockZipper) Open(rw http.ResponseWriter) {
+	m.Called(rw)
+}
+
+func (m *MockZipper) Close() error {
+	args := m.Called()
+	return args.Error(0)
+}
+
+func (m *MockZipper) AddFile(f *storage.File) error {
+	args := m.Called(f)
+	return args.Error(0)
 }
