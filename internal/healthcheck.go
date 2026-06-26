dee628f31e9d42cb2e4e0355fa3af3af98f1965c
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2026-06-26 13:01:11 +0100

    Replace opg-go-healthcheck with internal command
    `opg-go-healthcheck` is no longer supported, so create a function with matching behaviour inside opg-file-service.
    
    When the app is run, it checks if the `-hc` flag has been provided. If so, it prints "OK" or "FAIL" and exits 0 or 1.
    
    #patch

---
+++ internal/healthcheck.go
@@ -0,0 +1,32 @@
+package internal
+
+import (
+	"flag"
+	"net/http"
+	"os"
+)
+
+func RunHealthcheck(addr string) {
+	f := flag.Bool("hc", false, "perform a health check")
+	flag.Parse()
+
+	if !*f {
+		return
+	}
+
+	resp, err := http.Get(addr)
+	if err != nil {
+		os.Stdout.Write([]byte("FAIL: ERROR"))
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		os.Stdout.Write([]byte("FAIL: INVALID STATUS CODE"))
+		os.Exit(1)
+	}
+
+	os.Stdout.Write([]byte("OK"))
+	os.Exit(0)
+}
